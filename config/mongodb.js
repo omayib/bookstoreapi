b4a9a3c30952a401307070e1d22c4e2eb68e173c
akbarul <omayib@hepicar-mbp.local>
2019-11-26 18:54:27 +0700

    perbaikan env

--- config/mongodb.js
+++ config/mongodb.js
@@ -4,4 +4,5 @@ db.on('error', console.error.bind(console, 'connection db error:'));
 db.once('open', function() {
   console.log("connected to db")
 });
-mongoose.connect('mongodb+srv://siapa:saya@cluster0-zb4kl.mongodb.net/bookstore?retryWrites=true&w=majority', { useNewUrlParser: true});
+
+mongoose.connect(process.env.mongodburi, { useNewUrlParser: true});
